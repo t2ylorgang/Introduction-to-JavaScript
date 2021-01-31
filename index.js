0ee9b751109097632ae41765a010c6cac299e95b
Taylor Olson <pipestay622@gmail.com>
2021-01-31 13:38:12 -0800

    task 3 passes but needs more work

--- index.js
+++ index.js
@@ -91,11 +91,11 @@ Do the following:
    3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-    /*add your code here*/
-}
+function dogYears(age) {
+  return (age*7)
+};
 
-
+dogYears(3);
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -124,9 +124,17 @@ Use the hungryDog function and feeding requirements below to do the following:
   NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-    /*add your code here*/
+function hungryDog(weight, age) {
+  if (weight <= 5 && age >= 1) {
+    return (0.05*weight)
+  } else if (weight > 5 && weight <= 10) {
+    return (0.04*weight)
+  } else if (weight > 10 && weight <= 15) {
+    return (0.03*weight)
+  } else if (weight > 15) {
+    return (0.02*weight)
   }
+};
 
 
 
